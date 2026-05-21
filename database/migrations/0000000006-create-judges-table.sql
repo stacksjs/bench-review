c366973137cea9e13c2bcc86d8f90b878c856c75
glennmichael123 <gtorregosa@gmail.com>
2026-05-21 21:08:19 +0800

    chore: wip

--- database/migrations/0000000006-create-judges-table.sql
+++ database/migrations/0000000006-create-judges-table.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS "judges" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "name" TEXT,
   "image_url" TEXT,
+  "practice_area" TEXT CHECK ("practice_area" IN ('criminal', 'civil', 'family', 'probate', 'appellate', 'bankruptcy', 'other')),
   "court_house_id" INTEGER,
   "created_at" TEXT not null default CURRENT_TIMESTAMP,
   "updated_at" TEXT,
